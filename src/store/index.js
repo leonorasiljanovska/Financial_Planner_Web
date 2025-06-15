332601e83425ae711b8ed6228df24fd006c95580
Силјановска Леонора <leonora.siljanovska@students.finki.ukim.mk>
2025-06-15 15:15:33 +0200

    export_to_csv

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,3 @@
-// import { createStore } from 'vuex'
-//
-// export default createStore({
-//   state: {
-//     user: null,
-//     income: 0,
-//     expenses: [],
-//     currentMonth: new Date().getMonth(),
-//   },
-//   mutations: {
-//     setUser(state, user) {
-//       state.user = user
-//     },
-//     setIncome(state, income) {
-//       state.income = income
-//     },
-//     addExpense(state, expense) {
-//       state.expenses.push(expense)
-//     },
-//     deleteExpense(state, index) {
-//       state.expenses.splice(index, 1)
-//     },
-//   },
-//   getters: {
-//     savings: state => state.income * 0.15,
-//     spendable: state => state.income * 0.85 - state.expenses.reduce((sum, e) => sum + e.amount, 0),
-//     expensesByCategory: state => {
-//       return state.expenses.reduce((acc, exp) => {
-//         acc[exp.category] = (acc[exp.category] || 0) + exp.amount
-//         return acc
-//       }, {})
-//     }
-//   }
-// })
 
 import { createStore } from 'vuex'
 
